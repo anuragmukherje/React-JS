199c7e532be73511ffea22c492a24d372067a19b
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-21 10:54:15 +0530

    Updating the Module-2

---
+++ Module-2/jsx-module/src/components/JSXRules.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JSXRules = () => {
+  return (
+    <div>
+        <h1>JSXRules</h1>
+
+        <p>
+            <li>JSX must return a <em>single parent element</em></li>
+            <li>JSX elements must be <em>properly closed</em></li>
+            <li>JSX attributes are written using <em>camelCase</em> (e.g, <em>className</em> instead of <em>class</em>)</li>
+        </p>
+    </div>
+  )
+}
+
+export default JSXRules
